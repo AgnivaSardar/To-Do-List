ee261eb644c72a31b214318b7dfae90070443064
First-Repository <agniva.sardar@gmail.com>
2025-09-01 19:24:04 +0530

    Added Getters and Setters

--- src/main/java/com/todolist/demo/Task.java
+++ src/main/java/com/todolist/demo/Task.java
@@ -1,5 +1,52 @@
 package com.todolist.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Task {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+    private String title;
+    private String description;
+    private String flag;
+    private String colour;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
